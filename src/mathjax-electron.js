ddc02c1f97584182f5bf8b92f1bed7c0209b6557
Lukas Geiger <lukas.geiger94@gmail.com>
2016-07-14 10:09:48 +0200

    Export by name

--- src/mathjax-electron.js
+++ src/mathjax-electron.js
@@ -2,67 +2,66 @@
 
 var path = require('path');
 
-module.exports = {
-  /**
-   * Loads and configures MathJax if necessary.
-   * @param  {Document} document - A Document Object Model.
-   * The MathJax Script is included in the <head> section of the HTML document.
-   * @param  {Callback} callback - A callback to run when MathJax is loaded.
-   */
-  loadMathJax: function(document, callback) {
-    callback = (typeof callback === 'function') ? callback : function() {};
-    if (typeof MathJax === "undefined" || MathJax === null) {
-      var script = document.createElement("script");
+/**
+ * Loads and configures MathJax if necessary.
+ * @param  {Document} document - A Document Object Model.
+ * The MathJax Script is included in the <head> section of the HTML document.
+ * @param  {Callback} callback - A callback to run when MathJax is loaded.
+ */
+var loadMathJax = function(document, callback) {
+  callback = (typeof callback === 'function') ? callback : function() {};
+  if (typeof MathJax === "undefined" || MathJax === null) {
+    var script = document.createElement("script");
 
-      script.addEventListener("load", function() {
-        configureMathJax();
-        callback();
-      });
-      script.type = "text/javascript";
-
-      try {
-        script.src = path.join(__dirname, "..", "resources", "MathJax",
-        "MathJax.js?delayStartupUntil=configured");
-
-        document.getElementsByTagName("head")[0].appendChild(script);
-      } catch (error) {
-        throw new Error(error.message, "loadMathJax");
-      }
-    } else {
+    script.addEventListener("load", function() {
+      configureMathJax();
       callback();
-    }
-  },
-
-  /**
-   * Typesets any math elements within the element.
-   * @param  {HTMLElement}  container - The element whose math is to be typeset.
-   * @param  {Callback}     callback  - A callback to run when the typeset
-   * is complete.
-   */
-  typesetMath: function(container, callback) {
-    callback = (typeof callback === 'function') ? callback : function() {};
-    try {
-      MathJax.Hub.Queue(["Typeset", MathJax.Hub, container], callback);
-    } catch (error) {
-      throw new Error(error.message, "typesetMath");
-    }
-  },
-
-  /**
-   * A helper function which loads MathJax if necessary and typesets any math
-   * elements within the container.
-   * @param  {Document}     document  - A Document Object Model.
-   * The MathJax Script is included in the <head> section of the HTML document.
-   * @param  {HTMLElement}  container - The element whose math is to be typeset.
-   * @param  {Callback}     callback  - A callback to run when the typeset
-   * is complete.
-   */
-  mathProcessor: function(document, container, callback) {
-    this.loadMathJax(document, function() {
-      module.exports.typesetMath(container, callback);
     });
+    script.type = "text/javascript";
+
+    try {
+      script.src = path.join(__dirname, "..", "resources", "MathJax",
+      "MathJax.js?delayStartupUntil=configured");
+
+      document.getElementsByTagName("head")[0].appendChild(script);
+    } catch (error) {
+      throw new Error(error.message, "loadMathJax");
+    }
+  } else {
+    callback();
   }
 };
+
+/**
+ * Typesets any math elements within the element.
+ * @param  {HTMLElement}  container - The element whose math is to be typeset.
+ * @param  {Callback}     callback  - A callback to run when the typeset
+ * is complete.
+ */
+var typesetMath = function(container, callback) {
+  callback = (typeof callback === 'function') ? callback : function() {};
+  try {
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, container], callback);
+  } catch (error) {
+    throw new Error(error.message, "typesetMath");
+  }
+};
+
+/**
+ * A helper function which loads MathJax if necessary and typesets any math
+ * elements within the container.
+ * @param  {Document}     document  - A Document Object Model.
+ * The MathJax Script is included in the <head> section of the HTML document.
+ * @param  {HTMLElement}  container - The element whose math is to be typeset.
+ * @param  {Callback}     callback  - A callback to run when the typeset
+ * is complete.
+ */
+var mathProcessor = function(document, container, callback) {
+  loadMathJax(document, function() {
+    typesetMath(container, callback);
+  });
+};
+
 
 var configureMathJax = function() {
   MathJax.Hub.Config({
@@ -92,3 +91,9 @@ var configureMathJax = function() {
   });
   MathJax.Hub.Configured();
 };
+
+module.exports = {
+  loadMathJax,
+  typesetMath,
+  mathProcessor
+}
