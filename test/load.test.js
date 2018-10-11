75703579acd0f173c32fb5c5e42823172e3b6e69
Lukas Geiger <lukas.geiger94@gmail.com>
2018-10-11 13:40:25 +0200

    Update tests with snapshots

--- test/load.test.js
+++ test/load.test.js
@@ -1,44 +1,32 @@
-var assert = require("assert");
-var path = require("path");
-var mathJaxHelper = require("../index");
+const assert = require("assert");
+const snapshot = require("snap-shot");
+const { typesetMath, loadMathJax, mathJaxPath } = require("../index");
 
-describe("loading", function() {
-  it("should load MathJax", function(done) {
-    var mathJaxPath = path.join(__dirname, "..", "resources");
-    var headScript =
-      '<script type="text/javascript" src="' +
-      mathJaxPath +
-      '/MathJax/MathJax.js?config=electron"></script>';
+describe("loading", () => {
+  it("should load MathJax", done => {
+    const headScript = `<script type="text/javascript" src="${mathJaxPath}"></script>`;
     assert.ok(typeof MathJax === "undefined", "MathJax should be undefined");
-    mathJaxHelper.loadMathJax(document, function() {
+    loadMathJax(document, () => {
       assert.ok(typeof MathJax === "object", "MathJax should be defined");
-      var script = document.getElementsByTagName("script")[1].outerHTML;
+      const script = document.getElementsByTagName("script")[1].outerHTML;
       assert.equal(script, headScript);
 
       //It would throw if it tries to load MathJax again because loadMathJax is called without document
-      assert.doesNotThrow(mathJaxHelper.loadMathJax);
+      assert.doesNotThrow(loadMathJax);
 
       done();
     });
   });
 
-  it("should output the correct MathJax script", function(done) {
-    var math = "\\sum\\limits_{i=0}^{\\infty} \\frac{1}{n^2}";
-    var latex = "$$" + math + "$$";
+  it("should output the correct MathJax script", done => {
+    const math = "\\sum\\limits_{i=0}^{\\infty} \\frac{1}{n^2}";
+    const latex = "$$" + math + "$$";
 
-    var container = document.createElement("div");
+    const container = document.createElement("div");
     container.innerHTML = latex;
     assert.ok(typeof MathJax === "object", "MathJax should be defined");
-    mathJaxHelper.typesetMath(container, function() {
-      assert.equal(
-        container.getElementsByClassName("MathJax_SVG_Display").length,
-        1
-      );
-      assert.equal(container.getElementsByClassName("MathJax_SVG").length, 1);
-      assert.equal(
-        container.getElementsByTagName("script")[0].textContent,
-        math
-      );
+    typesetMath(container, () => {
+      snapshot(container.innerHTML);
       done();
     });
   });
