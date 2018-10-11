078bb405e7a39a133a2ef46d4c4272ad4c69983e
Lukas Geiger <lukas.geiger94@gmail.com>
2018-10-11 13:40:25 +0200

    Update tests with snapshots

--- test/load-and-typeset.test.js
+++ test/load-and-typeset.test.js
@@ -1,26 +1,19 @@
-var assert = require("assert");
-var path = require("path");
-var mathJaxHelper = require("../index");
+const assert = require("assert");
+const snapshot = require("snap-shot");
 
-describe("load and typeset", function() {
-  it("should should load and typeset", function(done) {
-    var math = "\\sum\\limits_{i=0}^{\\infty} \\frac{1}{n^2}";
-    var latex = "$$" + math + "$$";
+const { loadAndTypeset } = require("../index");
 
-    var container = document.createElement("div");
+describe("load and typeset", () => {
+  it("should should load and typeset", done => {
+    const math = "\\sum\\limits_{i=0}^{\\infty} \\frac{1}{n^2}";
+    const latex = "$$" + math + "$$";
+
+    const container = document.createElement("div");
     container.innerHTML = latex;
     assert.ok(typeof MathJax === "undefined", "MathJax should be undefined");
-    mathJaxHelper.loadAndTypeset(document, container, function() {
+    loadAndTypeset(document, container, () => {
       assert.ok(typeof MathJax === "object", "MathJax should be defined");
-      assert.equal(
-        container.getElementsByClassName("MathJax_SVG_Display").length,
-        1
-      );
-      assert.equal(container.getElementsByClassName("MathJax_SVG").length, 1);
-      assert.equal(
-        container.getElementsByTagName("script")[0].textContent,
-        math
-      );
+      snapshot(container.innerHTML);
       done();
     });
   });
